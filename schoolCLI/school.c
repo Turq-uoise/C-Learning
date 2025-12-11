0453217b4955cef48d0fb8bc17257847f8078fdc
Turq-uoise <rahim.mahmood@sky.uk>
2025-12-11 23:48:21 +0000

    Added plan
    Added student structure + grades and subjects

--- schoolCLI/school.c
+++ schoolCLI/school.c
@@ -3,28 +3,57 @@
 #include <stdbool.h>
 #include <string.h>
 
-enum SUBJECT {
+typedef enum {
     ENGLISH,
     MATHS,
     HISTORY,
     GEOGRAPHY,
     SCIENCE
-};
+} SUBJECT;
 
 const char* subjectNames[] = {"English", "Maths", "History", "Geography", "Science"};
 
+typedef enum {
+   A, B, C, D, E, U
+} GRADE;
+
+const char* grades[] = {"A", "B", "C", "D", "E", "U"};
+
+// Structure:
+// Teacher has name + subject
+// Student has list of subjects
+// Each subject in list has grade
+// Operations:
+// 1) Search students by subject: 
+// Go through each student in Students list -> Go through subjects they studies -> If subject is present, return student
+// 2) Search teacher by subject:
+// Search for subject in Teachers list, if it is present then return teacher
+// 3) Find grade student got in specific subject:
+// Pick student; Pick subject; Return grade
+// Bonus; 4) Search for teachers that teach a given student:
+// Go through each subject student studies -> Run 2)
+// 5) Find students who are taught by given teacher, i.e taught by subject
+// Run 1) -> Run 2)
+
+typedef struct SubjectList{
+    SUBJECT subject;
+    GRADE grade;
+    struct SubjectList* next;
+} SubjectList;
+
 typedef struct Teachers {
     char name[30];
-    enum SUBJECT subject;
+    SUBJECT subject;
     struct Teachers* next;
 } Teachers;
 
 typedef struct Students {
-    int data;
+    char name[30];
+    SubjectList subjects;
     struct Students* next;
 } Students;
 
-Teachers* create_Teachers(char name[30], enum SUBJECT subject) {
+Teachers* create_Teachers(char name[30], SUBJECT subject) {
     Teachers* new_Teachers = (Teachers*)malloc(sizeof(Teachers));
     if (!new_Teachers) {
         fprintf(stderr, "Memory allocation failed\n");
@@ -37,13 +66,13 @@ Teachers* create_Teachers(char name[30], enum SUBJECT subject) {
     return new_Teachers;
 }
 
-void push_front(Teachers** head, char name[30], enum SUBJECT subject) {
+void push_front(Teachers** head, char name[30], SUBJECT subject) {
     Teachers* new_Teachers = create_Teachers(name, subject);
     new_Teachers->next = *head;
     *head = new_Teachers;
 }
 
-void add_teacher(Teachers** head, char name[30], enum SUBJECT subject) {
+void add_teacher(Teachers** head, char name[30], SUBJECT subject) {
     Teachers* new_Teachers = create_Teachers(name, subject);
 
     if (*head == NULL) {
